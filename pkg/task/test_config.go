4044674bdc1104c94d34b3f8169456adafd645ff
austin <austinchang4@gmail.com>
2022-12-19 15:47:52 +0800

    test(task): parse config file

---
+++ pkg/task/test_config.go
@@ -0,0 +1,17 @@
+package task
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	t.Run("Parse Toml config file", func(t *testing.T) {
+		task, err := ParseFile("../../testdata/task.toml")
+		assert.NoError(t, err)
+		assert.Equal(t, "https://www.google.com", task.URL)
+		assert.Equal(t, "GET", task.Method)
+		assert.Equal(t, []uint32{200}, task.AcceptedStatusCodes)
+	})
+}
