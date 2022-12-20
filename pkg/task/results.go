419f0ac9d7a7193f9a0a657e4fdd9be2e509cecf
austin <austinchang4@gmail.com>
2022-12-20 09:41:24 +0800

    fix: total transfer mib render

--- pkg/task/results.go
+++ pkg/task/results.go
@@ -34,7 +34,7 @@ type Results struct {
 
 // Render the results
 func (r *Results) Render() {
-	fmt.Printf("\nTotal Duration: %v\nAvg. Duration %v\nMin. Duration %v\nMax Duration %v\nSuccess: %d\nFailed: %d\nTotal Transfer %.2f Mib\nThroughput: %.2f MiB/sec\nRequests/sec %2.f", r.Duration, r.AverageDuration, r.MinDuration, r.MaxDuration, r.SuccessCount, r.FailedCount, (r.TotalTransfer / 10000000), (r.Throughput / 1000000), r.RequestsPerSec)
+	fmt.Printf("\nTotal Duration: %v\nAvg. Duration %v\nMin. Duration %v\nMax Duration %v\nSuccess: %d\nFailed: %d\nTotal Transfer %.2f Mib\nThroughput: %.2f MiB/sec\nRequests/sec %2.f", r.Duration, r.AverageDuration, r.MinDuration, r.MaxDuration, r.SuccessCount, r.FailedCount, (r.TotalTransfer / 1000000), (r.Throughput / 1000000), r.RequestsPerSec)
 }
 
 // ExportResponsesToFile exports the responses to a CSV file
